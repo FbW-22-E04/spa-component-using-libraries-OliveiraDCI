1f0a90820c26704d2ab4541a14dd4407c3cc153a
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-11-07 14:17:31 +0100

    DCI solution.

---
+++ src/components/ProviderList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ProviderList({ foundProviders }) {
+  return (
+    <ul>
+      {foundProviders.map((provider, index) => (
+        <li key={index}>
+          {provider.name} {provider.location.city}
+        </li>
+      ))}
+    </ul>
+  );
+}
---
+++ src/filter.js
@@ -0,0 +1,8 @@
+export default function filterList(allEntries, searchTerm) {
+  const filteredArray = allEntries.filter((item) => {
+    return item.location.city
+      .toLocaleLowerCase()
+      .includes(searchTerm.toLocaleLowerCase());
+  });
+  return filteredArray;
+}
